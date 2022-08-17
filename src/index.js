dfb0d0955d2b54c98a1d42ddcd6a21184859f7f3
scottdavenport <scott@pinpoint.com>
2022-08-17 11:31:29 -0400

    approval card and children

--- src/index.js
+++ src/index.js
@@ -1,29 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CommentDetail from './CommentDetail';
 import { faker } from '@faker-js/faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
 	return (
 		<div className="ui container comments">
-			<CommentDetail
-				author="Sam"
-				timeAgo="Today at 4:42PM"
-				content="Check out my pictures!"
-				avatar={faker.image.avatar()}
-			/>
-			<CommentDetail
-				author="Alex"
-				timeAgo="Today at 2:00AM"
-				content="What a great weekend!"
-				avatar={faker.image.avatar()}
-			/>
-			<CommentDetail
-				author="Jane"
-				timeAgo="Yesterday at 6:23AM"
-				content="Love my family!!!"
-				avatar={faker.image.avatar()}
-			/>
+			<ApprovalCard>
+				<CommentDetail
+					author="Sam"
+					timeAgo="Today at 4:42PM"
+					content="Check out my pictures!"
+					avatar={faker.image.avatar()}
+				/>
+			</ApprovalCard>
+			<ApprovalCard>
+				<CommentDetail
+					author="Alex"
+					timeAgo="Today at 2:00AM"
+					content="What a great weekend!"
+					avatar={faker.image.avatar()}
+				/>
+			</ApprovalCard>
+			<ApprovalCard>
+				<CommentDetail
+					author="Jane"
+					timeAgo="Yesterday at 6:23AM"
+					content="Love my family!!!"
+					avatar={faker.image.avatar()}
+				/>
+			</ApprovalCard>
 		</div>
 	);
 };
