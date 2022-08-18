d1449aec9a871f28b905bc545acb32d646a35314
Scott Davenport <sdavenport@shopmonkey.io>
2022-08-18 13:23:43 -0400

    props and componenets

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { faker } from '@faker-js/faker';
+import './style/App.css';
 import CommentDetail from './CommentDetail';
 import ApprovalCard from './ApprovalCard';
 
